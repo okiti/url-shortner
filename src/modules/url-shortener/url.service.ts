6e7e26b6804d264d24ab7bd8d4aee0a377ab97d9
okiti <osivwiokiti@gmail.com>
2022-02-27 08:17:19 +0100

    Add url module

---
+++ src/modules/url-shortener/url.service.ts
@@ -0,0 +1,3 @@
+export const endcode = async () => {
+  // fee
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 router.get('/', (req, res, next) => {
-  res.send('Antantica is freezing. ..🥶 ...');
+  res.send('URL shortener API');
 });
 
 export default router;
