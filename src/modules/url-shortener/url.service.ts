48a58a5b72e97aa11baafb0b01e426651893633a
okiti <osivwiokiti@gmail.com>
2022-02-27 09:59:27 +0100

    Add url decode service

--- src/modules/url-shortener/url.service.ts
+++ src/modules/url-shortener/url.service.ts
@@ -1,6 +1,7 @@
 import { getConnection } from 'typeorm';
 import { Urls } from './entities/urls.entities';
 import shortCode from 'src/utils/generateCode';
+import HttpError from '../../exceptions/http-error';
 
 export const endcode = async (payload: { url: string }) => {
   const urlRepository = getConnection().getRepository(Urls);
@@ -13,4 +14,14 @@ export const endcode = async (payload: { url: string }) => {
 
   await shortendUrl.save();
   return shortendUrl;
+};
+
+export const decode = async (payload: { alias: string }) => {
+  const { alias } = payload;
+  const urlRepository = getConnection().getRepository(Urls);
+  const url = await urlRepository.findOne({ alias });
+  if (!url) {
+    throw new HttpError(404, `Url with alias ${alias} not found`);
+  }
+  return url;
 };
