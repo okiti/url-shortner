9f3c1546b368fc98289adf99504205da816278ca
okiti <osivwiokiti@gmail.com>
2022-02-28 00:09:20 +0100

    Add device logger middleware

---
+++ src/middleware/deviceLogger.ts
@@ -0,0 +1,20 @@
+import requestIp from 'request-ip';
+import dotenv from 'dotenv';
+import geoip from 'geoip-lite';
+import DeviceDetector from 'node-device-detector';
+
+dotenv.config();
+
+export const deviceLogger = async (req, res, next) => {
+  const userAgent = req.headers['user-agent'];
+  const clientIp =
+    process.env.NODE_ENV === 'development'
+      ? '105.112.102.139'
+      : requestIp.getClientIp(req);
+  const geo = geoip.lookup(clientIp);
+  const detector = new DeviceDetector();
+  const thisDevice = detector.detect(userAgent);
+  res.locals.geo = geo;
+  res.locals.device = thisDevice;
+  next();
+};
