d1ab8185af3efe9267b5dd815eeb6929a6cba551
okiti <osivwiokiti@gmail.com>
2022-02-27 08:09:49 +0100

    Initial setup and config

---
+++ src/config/vars.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+type varsDto = {
+  port?: string;
+  secret: string;
+  frontendUrl: string;
+};
+
+export const Vars: varsDto = {
+  port: process.env.PORT,
+  secret: process.env.SECRET,
+};
+
+export const isTestEnv = () => process.env.NODE_ENV === 'test';
