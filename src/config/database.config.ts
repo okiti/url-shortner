e46f9ab5926156f470245e28f05b02f7ac6778b1
okiti <osivwiokiti@gmail.com>
2022-02-28 12:45:59 +0100

    Update DB config to cover dev, test, and prod env

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,9 +3,9 @@ import { Vars, dbConfigVars } from './vars';
 export const getDbConfig = {
   host: 'localhost',
   port: 5432,
-  username: 'codelitt',
-  password: 'codelitt',
-  database: 'antarctica',
+  username: dbConfigVars[Vars.env].username,
+  password: dbConfigVars[Vars.env].password,
+  database: dbConfigVars[Vars.env].database,
   synchronize: true,
   logging: false,
   entities: dbConfigVars[Vars.env].entities,
